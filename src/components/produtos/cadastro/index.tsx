ea97c006d86834b7bfa57b69d5e3e01c5863f9bd
RubeniaLima <rubenia.lima2608@gmail.com>
2022-05-08 09:32:20 -0300

    Alinhando Campos do Formulario

--- src/components/produtos/cadastro/index.tsx
+++ src/components/produtos/cadastro/index.tsx
@@ -1,58 +1,59 @@
 import{Layout} from 'components'
 
- export const CadastroProdutos: React.FC = ()=> {
-    return(
-        <Layout titulo=" Produtos">
-            <div className='field'>
-                <label className='label' htmlFor='inputSku'>SKU: *</label>
-                <div className='control'>
-                    <input className='input' 
-                    id="inputSku" 
-                    placeholder='Digite o SKU do produto'/>
-                </div>
-
+export const CadastroProdutos: React.FC = ()=> {
+return(
+    <Layout titulo=" Produtos">
+        <div className='columns'>
+            <div className='field is-half column'>
+                    <label className='label' htmlFor='inputSku'>SKU: *</label>
+                    <div className='control'>
+                        <input className='input' 
+                            id="inputSku" 
+                            placeholder='Digite o SKU do produto'/>
+                    </div>
             </div>
 
-            <div className='field'>
+            <div className='field is-half column'>
                 <label className='label' htmlFor='inputPreco'>Preço: *</label>
                 <div className='control'>
                     <input className='input' 
-                    id="inputPreco" 
-                    placeholder='Digite o Preço do produto'/>
+                        id="inputPreco" 
+                        placeholder='Digite o Preço do produto'/>
                 </div>
-
             </div>
+         </div>
 
-            <div className='field'>
+        <div className='columns'>
+            <div className='field column is-full'>
                 <label className='label' htmlFor='inputNome'>Nome: *</label>
                 <div className='control'>
                     <input className='input' 
                     id="inputNome" 
                     placeholder='Digite o Nome do produto'/>
                 </div>
-
-                <div className='field'>
-                <label className='label' htmlFor='inputDesc'>Descrição: *</label>
-                <div className='control'>
-                    <textarea className='textarea' 
-                    id="inputDesc" 
-                    placeholder='Digite a Descrição detalhada do produto'/>
-                </div>
-
             </div>
-            <div className='field is-grouped'>
-                <div className='control is-link'>
-                    <button className='button'>Salvar</button>
-                </div>
-                <div className='control'>
-                    <button className='button'>Voltar</button>
-                </div>
+    </div>
 
-            </div>
-
+            <div className='columns'>
+                <div className='field column is-full'>
+                    <label className='label' htmlFor='inputDesc'>Descrição: *</label>
+                        <div className='control'>
+                            <textarea className='textarea' 
+                            id="inputDesc" 
+                            placeholder='Digite a Descrição detalhada do produto'/>
+                        </div>
+                </div>
+         </div>
+  
+        <div className='field is-grouped'>
+            <div className='control is-link'>
+                <button className='button'>Salvar</button>
             </div>
             
-            
+            <div className='control'>
+                <button className='button'>Voltar</button>
+            </div>
+        </div>  
 
         </Layout>
 
